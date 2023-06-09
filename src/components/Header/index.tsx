31e0d26d90a8c79df6b87f0ee2f1604f1b88ec7d
markoclimako <mkclimako@gmail.com>
2023-06-09 16:42:37 -0300

    style: header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import { Title } from '../Title';
+export function Header() {
+  return (
+    <header className="header">
+      <Title type="h1" title="Gerenciador de senhas" />
+    </header>
+  );
+}
